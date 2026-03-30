93894dcc4011f00d699edf7f954040a739c612a4
l4nszxc <hernandezlanslorence@gmail.com>
2026-03-30 18:55:43 +0800

    zxc

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.sjd0njhrbng"
+    "revision": "0.2ul0pmrra1"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
