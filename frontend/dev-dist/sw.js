5c9efbc8ff550780c32e5c7d5819df1f6528aad7
User <user@example.com>
2026-05-07 19:50:34 +0800

    Refactor code structure for improved readability and maintainability

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.pbctdjdetp"
+    "revision": "0.6mj4l9uobs8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
