f84cb3d70d5e5a3680a1274b3c42a0662d6caece
User <user@example.com>
2026-05-07 10:45:52 +0800

    Update revision for index.html in service worker precaching

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.dlv9n5utk0k"
+    "revision": "0.pbctdjdetp"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
